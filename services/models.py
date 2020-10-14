ec237a54bf779e0e3d954ca03a6b05da9974ed89
rucpata <rucinska.patrycja@gmail.com>
2020-10-14 18:48:19 +0200

    added services_listing_pages

--- services/models.py
+++ services/models.py
@@ -6,6 +6,7 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class ServiceListingPage(Page):
+    tamplate = 'services/service_listing_page.html'
     subtitle = models.TextField(
         blank = True,
         max_length = 500,
@@ -14,6 +15,12 @@ class ServiceListingPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
     ]
+
+    def get_context(self, request,*args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        context['services'] =  ServicePage.objects.live().public()
+        return context
+
 
 class ServicePage(Page):
     tamplate = 'services/service_page.html'
