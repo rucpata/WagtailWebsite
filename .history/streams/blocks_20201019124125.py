2f6e1e83bf8ff6a4d0fc99fc670c3950dc3952a4
rucpata <rucinska.patrycja@gmail.com>
2020-10-19 13:50:31 +0200

    edit card_block

---
+++ .history/streams/blocks_20201019124125.py
@@ -0,0 +1,32 @@
+from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
+
+class TitleBlock(blocks.StructBlock):
+    text = blocks.CharBlock(
+        required = True,
+        elp_text='Tekst do wyświetlenia',
+    )
+    class Meta:
+        template = 'streams/title_block.html'
+        icon = 'edycja'
+        label = 'Tytuł'
+        help_text = 'Wyśrodkowany tekst do wyświetlenia na stronie.'
+
+
+class CardsBlock(blocks.StructBlock):
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ('title', blocks.CharBlock(max_length=100, help_text = 'Pogrubiony tytuł tej karty. Maksymalnie 100 znaków.')),
+                ('text', blocks.TextBlock(max_length=255, help_text='Opcjonalny tekst tej karty. Maksymalnie 255 znaków.')),
+                ('image', ImageChooserBlock(help_text = 'Obraz zostanie automatycznie przycięty o 570 na 370 pikseli')),
+                ('link_text', blocks.CharBlock(max_length=50, default='Więcej sz')),
+                ('interal_page', blocks.PageChooserBlock(required=False)),
+                ('internal_link', blocks.URLBlock(required=False))
+            ]
+        )
+    )
+    class Meta: 
+        template = 'streams/card_block.html'
+        icon = 'image'
+        label = 'Karty standardowe'
