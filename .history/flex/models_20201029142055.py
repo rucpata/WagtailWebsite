12a7c4c8d8028c767177d3705d98834d9207792f
rucpata <rucinska.patrycja@gmail.com>
2020-10-29 15:09:30 +0100

    RichText streamfield

---
+++ .history/flex/models_20201029142055.py
@@ -0,0 +1,10 @@
+from django.db import models
+from wagtail.core.models import Page
+
+
+class FlexPage(Page):
+
+    
+    class Meta:
+        verbose_name = 'Flex (misc) page'
+        verbose_name_plural = 'Flex (misc) pages'
---
+++ .history/flex/models_20201029142213.py
@@ -0,0 +1,25 @@
+from django.db import models
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+
+class FlexPage(Page):
+
+    body = StreamField([
+        ('title', blocks.TitleBlock()),   
+        ('cards', blocks.CardsBlock()),
+        ('image_and_text', blocks.ImageAndTextBlock()),
+        ('cta', blocks.CallToActionBlock()),
+        ('testimonial', SnippetChooserBlock(
+            target_model='testimonials.Testimonial',
+            template = 'streams/testimonial_block.html' 
+        )),
+        ('pricing_table', blocks.PricingTableBlock(table_options=new_table_options)),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        Stre
+    ]
+
+    class Meta:
+        verbose_name = 'Flex (misc) page'
+        verbose_name_plural = 'Flex (misc) pages'
